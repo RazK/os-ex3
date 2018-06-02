78c85437e705d9d6f343ba0060f0c6ac1abc6f60
shimonheimowitz <heimy4prez@gmail.com>
2018-06-02 12:53:07 +0300

    compiles nicely

---
+++ MapReduceFramework.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Shimon Heimowitz on 02/06/2018.
+//
+
+#include "MapReduceFramework.h"
+
+void emit2 (K2* key, V2* value, void* context){
+    return;
+}
+void emit3 (K3* key, V3* value, void* context){
+    return;
+}
+
+void runMapReduceFramework(const MapReduceClient& client,
+                           const InputVec& inputVec, OutputVec& outputVec,
+                           int multiThreadLevel){
+    return;
+}
+
